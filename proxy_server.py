c8be2c2381945d4f4b587a5087cd598a45a13d61
Mikelarg <murtazkorpaz@gmail.com>
2017-10-05 18:33:28 +0800

    - fix error with custom ports - fix not correct fonts(other resources) loading - fix replace regex

--- proxy_server.py
+++ proxy_server.py
@@ -13,9 +13,10 @@ import requests
 from bs4 import BeautifulSoup, Comment, Doctype
 from bs4.dammit import EntitySubstitution
 
-replace_regex = r"(?P<start>^|\s)(?P<word>\S{6})(?P<end>\s|$)"
-subst = r"\g<start>\g<word>™\g<end>"
-host = "127.0.0.1"
+replace_regex = r"\b(?P<word>[\w\-_]{6})\b"
+subst = r"\g<word>™"
+_desktop_port = 9999
+_mobile_port = 9998
 
 
 class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
@@ -47,7 +48,9 @@ class HabrProxyServer(BaseHTTPRequestHandler):
     def _set_headers(self, response, content):
         self.send_response(response.status_code)
         for header, value in response.headers.items():
-            if header not in ["Content-Encoding", "Connection", 'Transfer-Encoding']:
+            if (response.headers.get("Accept-Ranges", "") == "bytes"
+                or header not in ["Content-Encoding", "Connection",
+                                  'Transfer-Encoding']) and header != 'Content-Length':
                 self.send_header(header, value)
         self.send_header("Content-Encoding", "gzip")
         self.send_header("Content-length", str(len(content)))
@@ -82,24 +85,28 @@ class HabrProxyServer(BaseHTTPRequestHandler):
             headers["Referer"] = self.habr_host
             response = self._get_habr_data(self.path, self.command, headers=headers)
             encoding = response.encoding if response.encoding else "UTF-8"
+            mobile_host = "http://{0}:{1}".format(self.server.server_name, _mobile_port)
+            desktop_host = "http://{0}:{1}".format(self.server.server_name, _desktop_port)
             content = response.text
-            content = content.replace("document.location.href = url;",
-                                      'document.location.href = "http://{0}:9998";'.format(
-                                          host))  # If enter via mobile phone
-            content = content.replace("https://habrahabr.ru", "http://{0}:9999".format(host))
-            content = content.replace("http://habrahabr.ru", "http://{0}:9999".format(host))
-            content = content.replace("https://m.habrahabr.ru", "http://{0}:9998".format(host))
-            content = content.replace("http://m.habrahabr.ru", "http://{0}:9998".format(host))
-            if "text/html" in response.headers["Content-Type"]:
-                content = BeautifulSoup(content, 'lxml')
-                strings = content.find_all(text=self._search_words)
-                for string in strings:
-                    string.replaceWith(re.sub(replace_regex, subst, string, 0, re.DOTALL))
-                content = content.prettify(encoding=encoding, formatter=self._html_entities)
+            if response.headers.get("Accept-Ranges", None) != "bytes":
+                content = content.replace("document.location.href = url;",
+                                          'document.location.href = "{0}";'.format(
+                                              mobile_host))  # If enter via mobile phone
+                content = content.replace("https://habrahabr.ru", desktop_host)
+                content = content.replace("http://habrahabr.ru", desktop_host)
+                content = content.replace("https://m.habrahabr.ru", mobile_host)
+                content = content.replace("http://m.habrahabr.ru", mobile_host)
+                if "text/html" in response.headers.get("Content-Type", ""):
+                    content = BeautifulSoup(content, 'lxml')
+                    strings = content.find_all(text=self._search_words)
+                    for string in strings:
+                        string.replaceWith(re.sub(replace_regex, subst, string, 0, re.DOTALL))
+                    content = content.prettify(encoding=encoding, formatter=self._html_entities)
+                else:
+                    content = content.encode(encoding)
             else:
-                content = content.encode(encoding)
+                content = response.content
             content = self._gzipencode(content)
-
             self._set_headers(response, content)
             self.wfile.write(content)
             self.connection.close()
@@ -115,12 +122,15 @@ class HabrMobileProxyServer(HabrProxyServer):
 
 
 def run(server_class=ThreadedHTTPServer, handler_class=HabrProxyServer, handler_mobile_class=HabrMobileProxyServer,
-        port=9999, mobile_port=9998, host="127.0.0.1"):
-    server_address = (host, port)
+        desktop_port=9999, mobile_port=9998, host="127.0.0.1"):
+    global _desktop_port, _mobile_port
+    _desktop_port = desktop_port
+    _mobile_port = mobile_port
+    server_address = (host, desktop_port)
     mobile_server_address = (host, mobile_port)
     httpd = server_class(server_address, handler_class)
     mobile_httpd = server_class(mobile_server_address, handler_mobile_class)
-    print('Starting habr proxy at http://{0}:{1}'.format(host, port))
+    print('Starting habr proxy at http://{0}:{1}'.format(host, desktop_port))
     server_thread = threading.Thread(target=lambda server: server.serve_forever(), args=([httpd]))
     server_thread.start()
     print('Starting mobile habr proxy at http://{0}:{1}'.format(
@@ -135,12 +145,11 @@ if __name__ == "__main__":
     if len(argv) > 1:
         kwargs = {}
         if len(argv) > 1:
-            kwargs["port"] = int(argv[1])
+            kwargs["desktop_port"] = int(argv[1])
         if len(argv) > 2:
             kwargs["mobile_port"] = int(argv[2])
         if len(argv) > 3:
-            host = argv[3]
-            kwargs["host"] = host
+            kwargs["host"] = argv[3]
         run(**kwargs)
     else:
         run()
